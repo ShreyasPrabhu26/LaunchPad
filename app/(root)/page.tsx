8038c8b1ef6f8b5db9cf53319c350685ede706ac
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2025-02-25 15:32:11 +0530

    Refactor layout and navbar components, add search functionality, and update styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -11,6 +18,8 @@ export default function Home() {
           Submit your ideas, support top pitches, and shine in virtual
           competitions.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
